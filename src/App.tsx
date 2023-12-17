91d1e39463e1392dda7e7bfe61a998206bede0fa
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-12-17 18:09:28 -0300

    ADD header class progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './header';
 import ProductsList from './products';
 import ProductFormPage from './products_form';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProductsList />} />
-        <Route path="/product/:id" element={<ProductFormPage />} />
-        <Route path="/product/add" element={<ProductFormPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProductsList />} />
+            <Route path="/product/:id" element={<ProductFormPage />} />
+            <Route path="/product/add" element={<ProductFormPage />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
+
+
   );
 }
 
---
+++ src/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <strong>MCGA TP Final - Sanchez Palomino</strong>
+      <button className="login-button">Login</button>
+    </header>
+  );
+};
+
+export default Header;
