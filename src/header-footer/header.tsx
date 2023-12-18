3073fb0907318f64bf993aeb0b9768b657c7eb51
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-12-17 23:55:20 -0300

    ADD handle logout flow reset

--- src/header-footer/header.tsx
+++ src/header-footer/header.tsx
@@ -3,24 +3,32 @@ import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../utils/firebase/firebase';
 import './syle-sheet.css';
+import ConfirmationDialog from '../utils/dialog';
 
 const Header = () => {
   const navigate = useNavigate();
   const [userIsLogged, setUserIsLogged] = useState(false);
+  const [isLogoutDialogOpen, setIsLogoutDialogOpen] = useState(false);
 
   const handleButtonClick = () => {
     if (userIsLogged) {
-      logout();
+      setIsLogoutDialogOpen(true);
     } else {
       navigate('/login');
     }
   };
+
+  const handleLogoutCancel = () => {
+    setIsLogoutDialogOpen(false);
+  }
 
   const logout = () => {
     signOut(auth).then(() => {
       navigate("/");
     }).catch((error) => {
       console.log('error: ', error)
+    }).finally(() => {
+      setIsLogoutDialogOpen(false);
     });
   };
 
@@ -38,14 +46,28 @@ const Header = () => {
   }, [])
 
   return (
-    <header className='container'>
-      <strong>MCGA TP Final - Sanchez Palomino</strong>
-      <button
-        className={`login-button ${userIsLogged ? 'logout-button' : ''}`}
-        onClick={handleButtonClick}>
-        {userIsLogged ? 'Logout' : 'Login'}
-      </button>
-    </header>
+    <>
+      <header className='container'>
+        <strong>MCGA TP Final - Sanchez Palomino</strong>
+        <button
+          className={`login-button ${userIsLogged ? 'logout-button' : ''}`}
+          onClick={handleButtonClick}>
+          {userIsLogged ? 'Logout' : 'Login'}
+        </button>
+      </header>
+
+      {isLogoutDialogOpen && (
+        <ConfirmationDialog
+          isOpen={isLogoutDialogOpen}
+          title='¿Estás seguro que deseás cerrar sesión?'
+          cancelText='Volver'
+          onCancel={() => handleLogoutCancel()}
+          confirmText='Cerrar sesión'
+          onConfirm={() => logout()}
+        />
+      )}
+    </>
+
   );
 };
 
