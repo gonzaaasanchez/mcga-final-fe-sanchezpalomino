7f63c313712a456f1c042968baa9c58b9b844fd1
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-12-18 21:09:34 -0300

    FIX page name

--- src/utils/not-found.tsx
+++ src/utils/not-found.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 
-const NotFoundASD = () => {
+const NotFoundPage = () => {
     return <div>
         <h3>Página no encontrada</h3>
     </div>
 }
-export default NotFoundASD
+export default NotFoundPage
